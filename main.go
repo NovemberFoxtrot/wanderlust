e81ff5c2f93c0f10f658c2d8081aaed4d980c86d
November Foxtrot <november@fox.trot>
2014-02-28 23:50:57 +0900

    rr

--- main.go
+++ main.go
@@ -40,11 +40,18 @@ func process(path string) (string, bool) {
 
 	err = reader.Open(path)
 
+	models := make(map[string]int)
+
 	if err == nil {
-		for key, val := range reader.Tags {
-			fmt.Printf("%s: %s\n", key, val)
+		for key, value := range reader.Tags {
+			fmt.Printf("%s: %s\n", key, value)
+			if key == `Model` {
+				models[value] += 1
+			} 
 		}
 	}
+
+	fmt.Println(models)
 
 	return path, true
 }
